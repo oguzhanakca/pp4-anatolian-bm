91192c3ca72fd1643edae70c169c83f8a3955c39
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-02 01:06:10 +0200

    comment model created

--- blog/models.py
+++ blog/models.py
@@ -8,11 +8,17 @@ class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
