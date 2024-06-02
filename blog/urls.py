4d0433e8d60292c05e4e59d43eaf05f67654cfaf
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-02 22:27:47 +0200

    static css added, applied

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("posts/", views.PostList.as_view(), name="blog-home"),
+    path("", views.PostList.as_view(), name="blog_home"),
 ]
