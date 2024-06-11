636e83292663147dbe87d5d3d3dfc1f159dcb46b
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-11 14:51:50 +0200

    signup attribute error fixed

--- anatolian_bm/forms.py
+++ anatolian_bm/forms.py
@@ -2,20 +2,17 @@ from django import forms as d_forms
 from allauth.account.forms import SignupForm
 class CustomSignupForm(SignupForm):
 
+    first_name = d_forms.CharField(max_length = 25, label='First Name',required=True)
+    last_name = d_forms.CharField(max_length = 25, label='Last Name',required=True)
+
     def save(self, request):
 
         # Ensure you call the parent class's save.
         # .save() returns a User object.
         user = super(CustomSignupForm, self).save(request)
-
-        # Add your own processing here.
-    first_name = d_forms.CharField(max_length = 25, label='First Name')
-    last_name = d_forms.CharField(max_length = 25, label='Last Name')
-    def signup(self,request,user):
-            
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
-        user.save()
-
+        # Add your own processing here.
+    
         # You must return the original result.
         return user
