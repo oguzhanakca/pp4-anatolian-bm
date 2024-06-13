6811dc3fead789b7f2dcacb9295737b38997176b
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-13 15:42:24 +0200

    signup winerror 10061 fixed

--- anatolian_bm/forms.py
+++ anatolian_bm/forms.py
@@ -1,9 +1,10 @@
 from django import forms as d_forms
 from allauth.account.forms import SignupForm
+
 class CustomSignupForm(SignupForm):
 
-    first_name = d_forms.CharField(max_length = 25, label='First Name',required=True)
-    last_name = d_forms.CharField(max_length = 25, label='Last Name',required=True)
+    first_name = d_forms.CharField(max_length = 25, label='First Name', required=True)
+    last_name = d_forms.CharField(max_length = 25, label='Last Name', required=True)
 
     def save(self, request):
 
@@ -12,6 +13,7 @@ class CustomSignupForm(SignupForm):
         user = super(CustomSignupForm, self).save(request)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        
         # Add your own processing here.
     
         # You must return the original result.
--- anatolian_bm/settings.py
+++ anatolian_bm/settings.py
@@ -57,6 +57,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -122,9 +123,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 ACCOUNT_FORMS = {'signup': 'anatolian_bm.forms.CustomSignupForm'}
 
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
 ACCOUNT_USERNAME_MIN_LENGTH = 5
 ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "none"
 
 
 # Internationalization
