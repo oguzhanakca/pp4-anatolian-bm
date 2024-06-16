0eaafa2f8033a494644ee4188873e9c52ab454e0
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-16 16:21:20 +0200

    about carousel colored

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,3 +1,3 @@
-// Carousels slide with touchpad
+// Carousels slide with touchpad without clicking
 const story_carousel = new bootstrap.Carousel("#story_commitment");
 const the_bmb_carousel = new bootstrap.Carousel("#the_bmb");
