f9bd2f4a01b27b0c92f0070edb38a863210c64da
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-18 16:14:19 +0200

    prevent booking old dates

--- booking/forms.py
+++ booking/forms.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from django import forms
 from .models import Booking
 from django.core.exceptions import ValidationError
@@ -12,10 +12,16 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ['requested_date','requested_time','guests','message']
         widgets =  {
-            'requested_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'requested_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'min': datetime.now().date()}),
             'requested_time': forms.Select(attrs={'class': 'form-control'}),
             'guests': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'How many guests will be coming?'}),
             'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Please write down everything we need to know about your reservation.'})
         }
+
+    def clean_to_date(self):
+        data = self.cleaned_data['requested_date']
+        if data < datetime.now():
+            raise forms.ValidationError("'to' date cannot be later than today.")
+        return data
 
     
