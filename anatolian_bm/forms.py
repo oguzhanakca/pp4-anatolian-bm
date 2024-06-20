f24697a62b8d7764d2918172d559b022d90178de
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-20 23:57:53 +0200

    unique email check added to signup form

--- anatolian_bm/forms.py
+++ anatolian_bm/forms.py
@@ -1,5 +1,6 @@
 from django import forms as d_forms
-from allauth.account.forms import SignupForm, ResetPasswordForm, LoginForm, ResetPasswordKeyForm
+from allauth.account.forms import SignupForm, ResetPasswordForm, LoginForm
+from django.contrib.auth.models import User
 
 class CustomSignupForm(SignupForm):
     """
@@ -49,6 +50,12 @@ class CustomSignupForm(SignupForm):
         # Add your own processing here.
         # You must return the original result.
         return user
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise d_forms.ValidationError("A user with that email already exists.")
+        return email
     
 class CustomLoginForm(LoginForm):
     """
