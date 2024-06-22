f8b35a58028dfb2db2077f3610ce9d7cbc0d61e9
oguzhanakca <oguzhan.akca@hotmail.com>
2024-06-22 17:13:50 +0200

    booking list added to book page, booking_list page removed. Now book page checks if the user is staff or not

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,4 @@ from django.urls import path
 urlpatterns = [
     path("book", views.book, name="book"),
     path("booking-success", views.booking_success, name="booking_success"),
-    path("booking-list", views.booking_list, name="booking_list")
 ]
--- booking/views.py
+++ booking/views.py
@@ -5,14 +5,14 @@ from .forms import BookingForm
 from .models import Booking
 from .utils import send_booking_success_email
 
-def admin_check(user):
-    return user.is_staff
+@login_required
+def book(request):
+    """
+    If user logged in, shows the booking page.
+    If not, redirects to login page.
+    """
 
-@user_passes_test(admin_check)
-def booking_list(request):
-    """
-    Booking List for admin users
-    """
+    # Ordering
     order = request.GET.get('order', '-created_date')
     if order.startswith('-'):
         new_order_date = order[1:] if order == "-requested_date" else "requested_date"
@@ -25,31 +25,15 @@ def booking_list(request):
         new_order_guests = '-' + order if order == 'guests' else 'guests'
         new_order_status = '-' + order if order == 'status' else 'status'
 
-    booking_list = Booking.objects.all().order_by(order)
-    paginator = Paginator(booking_list, 15)  # Show 15 bookings per page.
-    page_number = request.GET.get("page")
-    paginated_list = paginator.get_page(page_number)
+    # User Role and Pagination
+    user = request.user
+    if user.is_staff:
+        bookings = Booking.objects.all().order_by(order)
+        paginator = Paginator(bookings, 15)  # Show 15 bookings per page.
+    else:
+        bookings = Booking.objects.filter(user=user).order_by(order)
+        paginator = Paginator(bookings, 5)  # Show 5 bookings per page.
 
-    return render(
-        request, 'booking/booking_list.html', {
-            'booking_list': paginated_list, 
-            'current_order': order, 
-            'new_order_date': new_order_date, 
-            'new_order_time': new_order_time, 
-            'new_order_guests': new_order_guests, 
-            'new_order_status': new_order_status, 
-            })
-
-@login_required
-def book(request):
-    """
-    If user logged in, shows the booking page.
-    If not, redirects to login page.
-    """
-
-    #Booking List
-    bookings = Booking.objects.filter(user=request.user).order_by("-requested_date")
-    paginator = Paginator(bookings, 5)  # Show 5 bookings per page.
     page_number = request.GET.get("page")
     paginated_list = paginator.get_page(page_number)
 
@@ -57,15 +41,26 @@ def book(request):
     form = BookingForm(data=request.POST)
     if form.is_valid():
         booking = form.save(commit=False)
-        booking.user = request.user
+        booking.user = user
         booking.save()
-        send_booking_success_email(request.user, booking)
+        send_booking_success_email(user, booking)
         return redirect('booking_success')
     else:
         form = BookingForm()
     
     return render(
-        request, 'booking/booking.html', {'form' : form, 'paginated_list' : paginated_list}
+        request, 
+        'booking/booking.html', 
+        {
+            'form' : form, 
+            'paginated_list' : paginated_list, 
+            'user': user, 
+            'current_order': order, 
+            'new_order_date': new_order_date, 
+            'new_order_time': new_order_time, 
+            'new_order_guests': new_order_guests, 
+            'new_order_status': new_order_status,
+        }
     )
 
 def booking_success(request):
