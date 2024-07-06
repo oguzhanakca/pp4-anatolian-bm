6074b1e87a931a8430b0c141e153922ff1a2cbf2
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-06 20:05:53 +0200

    comment on post added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
+from .forms import CommentForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 @login_required
@@ -10,11 +12,14 @@ def posts(request):
     """
     # Published posts
     posts = Post.objects.filter(status=1)
+    paginator = Paginator(posts, 15)
+    page_number = request.GET.get("page")
+    paginated_posts = paginator.get_page(page_number)
 
     return render(
         request, 
         "blog/posts.html", 
-        {"posts": posts},
+        {"posts": paginated_posts},
     )
 
 @login_required
@@ -22,15 +27,29 @@ def post_detail(request, id):
     """
     Display the post.
     """
-
     queryset = Post.objects.all()
     post = get_object_or_404(queryset, id=id)
     # Approved comments
-    comments = Comment.objects.filter(post_id=post.id, approved=True).order_by("created_at")
+    comments = Comment.objects.filter(post_id=post.id, approved=True).order_by("-created_at")
+    paginator = Paginator(comments, 15)
+    page_number = request.GET.get("page")
+    paginated_comments = paginator.get_page(page_number)
+    if request.method == 'POST':
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.author = request.user
+                comment.post = post
+                comment.save()
+                form.save()
+                return redirect('post_detail', id=post.id)  # Adjust the redirect as needed
+    else:
+        form = CommentForm()
 
     return render(
         request,
         "blog/post_detail.html",
         {"post": post,
-         "comments":comments},
+         "comments":paginated_comments,
+         "form":form},
     )
