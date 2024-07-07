800065e5093e11d5f2fa02505e77843996ad1d36
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-07 18:06:53 +0200

    shop page created

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
+@login_required
 def shop(request):
     """
     Display Shop homepage
