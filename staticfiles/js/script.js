32a8e562393bbe0556fccbd93999d2896fb0f737
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-07 17:36:06 +0200

    static collected, shop app created

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,3 +1,3 @@
-// Carousels slide with touchpad without clicking
+// Carousels slide with touchpad without requiring to touch first
 const story_carousel = new bootstrap.Carousel("#story_commitment");
 const the_bmb_carousel = new bootstrap.Carousel("#the_bmb");
