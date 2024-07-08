d6e89824c5fd0dda317caf0990c697e5220ccbd1
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-08 03:35:49 +0200

    product model created

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY = ((0, "Fruit and Vegetable"), (1, "Meat"), (2, "Seafood"),(3, "Frozen Products"), (4, "Drink"), (5, "Bakery"), (6, "Dairy"))
+
+class Product(models.Model):
+    """
+    Product Model
+    """
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    category = models.IntegerField(choices=CATEGORY)
+    description = models.TextField()
+    stock = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.id} : {self.name}"
