68938363a470d9653bc8fb1d17c55c590c9e387f
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-10 23:33:50 +0200

    shop carousel added, need to fix swipe

--- shop/models.py
+++ shop/models.py
@@ -7,13 +7,19 @@ class Product(models.Model):
     Product Model
     """
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
     category = models.IntegerField(choices=CATEGORY)
-    description = models.TextField()
+    image = models.ImageField(upload_to="product_images/")
+    price = models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
     stock = models.IntegerField(default=0)
+    sold = models.PositiveBigIntegerField(default=0)
+    on_sale = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["name"]
 
     def __str__(self):
-        return f"{self.id} : {self.name}"
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from .models import Product
 
 @login_required
 def shop(request):
@@ -8,4 +9,7 @@ def shop(request):
     Display Shop homepage
     """
 
-    return render(request,"shop/shop.html")
+    products = Product.objects.filter(on_sale=True)
+    most_sellers = products.order_by("-sold")[:6]
+
+    return render(request,"shop/shop.html",{"products": products,"most_sellers":most_sellers})
