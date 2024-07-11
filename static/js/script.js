2f44366ad8faf07a11189ba3ff573a60b9b68c9d
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-11 17:09:00 +0200

    carousel slide bug fixed

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 // Carousels slide with touchpad without requiring to touch first
 const story_carousel = new bootstrap.Carousel("#story_commitment");
 const the_bmb_carousel = new bootstrap.Carousel("#the_bmb");
-const most_seller_carousel = new bootstrap.Carousel("#most-sellers-carousel");
+const most_sellers = document.querySelector("#most-sellers-carousel");
+const most_seller_carousel = new bootstrap.Carousel(most_sellers);
