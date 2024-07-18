471492f86fb17df2b2a93f882bf98aeaf64db9dc
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-18 16:36:09 +0200

    contact form added

--- about/views.py
+++ about/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -6,4 +9,22 @@ def about(request):
     """
     Display About Us page
     """
-    return render(request,"about/about.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+            body = f"Name: {name}\nEmail : {email}\nMessage: {message}"
+            send_mail(
+                subject=f"Contact Message from {name}",
+                message=body,
+                from_email=email,
+                recipient_list=[settings.DEFAULT_FROM_EMAIL],
+            )
+
+            return redirect("about")
+    else:
+        form = ContactForm()
+
+    return render(request,"about/about.html",{"form":form})
