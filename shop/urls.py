d65a759d3e330f83b293c1037b98af5b643073b5
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-23 23:32:12 +0200

    product detail link added to shop homepage

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.shop, name="shop"),
+    path('<int:id>/', views.product_detail, name='product_detail'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from .models import Product
@@ -13,4 +13,14 @@ def shop(request):
     most_sellers = products.order_by("-sold")[:12]
     latest_products = products.order_by("-created_at")[:12]
 
-    return render(request,"shop/shop.html",{"products": products,"most_sellers":most_sellers,"latest_products":latest_products})
+    return render(request, "shop/shop.html",{"products": products,"most_sellers":most_sellers,"latest_products":latest_products})
+
+@login_required
+def product_detail(request, id):
+    """
+    Displays Product Detail page
+    """
+    queryset = Product.objects.all()
+    product = get_object_or_404(queryset, id=id)
+
+    return render(request, "shop/product-detail.html", {"product": product})
