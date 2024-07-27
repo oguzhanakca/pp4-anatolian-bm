aec16a3e6ca87229feea3152ab74830581793fbb
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-27 18:45:46 +0200

    wave validation - blog fixed

--- blog/forms.py
+++ blog/forms.py
@@ -22,6 +22,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['body']
+        labels = {
+            'body': "Send your comment"
+        }
         widgets = {
-            'body': forms.Textarea(attrs={'class':'form-control', 'rows': 3, 'placeholder': 'Enter your comment'}),
+            'body': forms.TextInput(attrs={'class':'form-control', 'rows': 3, 'placeholder': 'Enter your comment'}),
         }
