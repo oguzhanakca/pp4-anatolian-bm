e609fa02a045987d0c83b0fa28e9cc184f96480b
oguzhanakca <oguzhan.akca@hotmail.com>
2024-07-27 16:37:48 +0200

    settings, media direction changed

--- anatolian_bm/urls.py
+++ anatolian_bm/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path("booking/", include("booking.urls"), name='booking-urls'),
     path("shop/", include("shop.urls"), name='shop-urls'),
     path('summernote/', include("django_summernote.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
